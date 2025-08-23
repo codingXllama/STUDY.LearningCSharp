9694d4d5d365645a44dac0d139e747847a09d01c
codingxllama <osamaalchalabi2@gmail.com>
2025-08-23 16:20:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Please enter your age: ");
+           int age = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine($"Your age is {age}");
+           
+           // write a loop to check if items in the input is a string or int
+           foreach (int item in age)
+            {
+
+            }
+
+           
+        }
+    }
+}
