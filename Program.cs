944feaab8a359cbce2413e4ffefaa88d78c7eba5
codingxllama <osamaalchalabi2@gmail.com>
2025-08-23 16:24:19 -0400

    Inital Commit - Adding in Userinput, and output. 08232025

--- Program.cs
+++ Program.cs
@@ -9,12 +9,8 @@ namespace ConsoleApp1
            Console.Write("Please enter your age: ");
            int age = Convert.ToInt32(Console.ReadLine());
            Console.WriteLine($"Your age is {age}");
-           
-           // write a loop to check if items in the input is a string or int
-           foreach (int item in age)
-            {
-
-            }
+     
+           Console.WriteLine("Hello, C# Academy!");
 
            
         }
