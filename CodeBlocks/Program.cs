1734384ca3420e07ffd9abd6c5854328efa2c2be
codingxllama <osamaalchalabi2@gmail.com>
2025-08-26 23:21:47 -0400

    Control-coding exercise completed.

--- CodeBlocks/Program.cs
+++ CodeBlocks/Program.cs
@@ -1,19 +1,64 @@
 ﻿namespace CodeBlocks
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
 
-            bool flag = true;
-            int value;
-            if (flag)
+            //bool flag = true;
+            //int value;
+            //value = 10;
+            //if (flag)
+            //{
+            //    value = 10;
+            //    Console.WriteLine($"Inside the code block {value}");
+            //}
+
+            //Console.WriteLine($"Outside the code block: {value}");
+
+            //Console.WriteLine($"Inside the code block: {value}");
+            //Console.WriteLine($"Outside the code block {value}");
+            //IfStatementCodeBlock();
+
+            Exercise();
+
+
+        }
+
+
+        public static void IfStatementCodeBlock()
+        {
+            //bool flag = true;
+            //if (flag) Console.WriteLine(flag);
+
+            string name = "steve";
+            if (name == "bob") Console.WriteLine("Foun Bob");
+            else if (name.Contains("steve")) Console.WriteLine("Found Steve");
+            else Console.WriteLine("Found Chunk");
+
+        }
+
+        public static void Exercise()
+        {
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total = 0;
+            bool found = false;
+            foreach (int number in numbers)
             {
-                value = 10;
-                Console.WriteLine($"Inside the code block {value}");
+                total += number;
+                if (number == 42)
+                {
+                    found = true;
+                }
             }
 
-            Console.WriteLine($"Outside the code block: {value}");
+            if (found)
+            {
+                Console.WriteLine("Set contains 42");
+            }
+
+            Console.WriteLine($"Total: {total}");
+
         }
     }
 }
--- CodeBlocks/obj/Debug/net9.0/CodeBlocks.AssemblyInfo.cs
+++ CodeBlocks/obj/Debug/net9.0/CodeBlocks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeBlocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3d602474e45bd8272db168f427ff943b72afec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21a5a94d19b0694ec00ea32b5a1eff099773e30")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeBlocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeBlocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
