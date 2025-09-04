44e7d122fad1415486265027b3b98bbe58c37092
codingxllama <osamaalchalabi2@gmail.com>
2025-09-03 23:40:12 -0400

    Initial stage of the comment challenge

--- Comments/Program.cs
+++ Comments/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            CommentExample();
+            //CommentExample();
+            Challenge();
         }
 
         public static void CommentExample()
@@ -38,5 +39,31 @@
             }
         }
 
+
+
+        public static void Challenge()
+        {
+            string str = "The quick brown fox jumps over the lazy dog.";
+
+            //convert the message into a char array
+            char[] charMessage = str.ToCharArray();
+            // Reverse the chars
+            Array.Reverse(charMessage);
+            int x = 0;
+            // count the o's 
+            foreach (char i in charMessage)
+            {
+                if (i == 'o')
+                {
+                    x++;
+                }
+            }
+            //convert it back to a string
+            string new_message = new String(charMessage);
+
+            //print it out
+            Console.WriteLine(new_message);
+            Console.WriteLine($"'o' appears {x} times");
+        }
     }
 }
