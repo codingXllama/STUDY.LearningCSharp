79749a6b3eb1c260043ff34e71da821fa1779af9
codingxllama <osamaalchalabi2@gmail.com>
2025-09-08 22:50:02 -0400

    Implemented base code for Sophia example

--- Part2_GuidedProject/Program.cs
+++ Part2_GuidedProject/Program.cs
@@ -10,12 +10,29 @@
         // Exercise from https://learn.microsoft.com/en-us/training/modules/guided-project-arrays-iteration-selection/2-prepare
         public static void Exercise()
         {
-            int[] grades = new int[5];
+            int currentAssignments = 5;
+            int[] sophiaScores = new int[] { 90, 85, 87, 98, 100 };
+            int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+            int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+            int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-            for (int i = 0; i < 3; i++)
+            int sophiaSum = 0;
+            decimal sophiaScore;
+            sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+            Console.WriteLine("Student\t\tGrade\n");
+            Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+            // displaying each grade in Sophia socres
+            foreach (int score in sophiaScores)
             {
-
+                Console.WriteLine(score);
             }
+
+            Console.WriteLine("Press the Enter key to continue");
+            Console.ReadLine();
+
+
         }
     }
 }
